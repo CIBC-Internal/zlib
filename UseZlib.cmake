8c7a9ecbad16109096e98498a26a0a84758fae65
ayla <ayla@sci.utah.edu>
2015-04-10 15:11:04 -0600

    Turn on symbol mangling.

--- UseZlib.cmake
+++ UseZlib.cmake
@@ -1,3 +1,9 @@
+if(NOT Zlib_FOUND)
+  MESSAGE(FATAL_ERROR "Including UseZlib.cmake but the Zlib package was not found.")
+endif()
 
 include_directories(${SCI_ZLIB_INCLUDE})
 link_directories(${SCI_ZLIB_LIBRARY_DIR})
+if (${SCI_ZLIB_MANGLE})
+  add_definitions(-DZ_PREFIX)
+endif()
